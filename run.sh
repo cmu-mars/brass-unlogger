aac632b0a40d7d1d05b6fa7c418a6fe8afc90bee
Ian Voysey <iev@cs.cmu.edu>
2017-05-03 14:21:28 -0400

    run script

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-cat header.csv >> results.csv
+if ! [ -e "results.csv" ]
+then
+   cat header.csv >> results.csv
 
-for d in initialTests/*
-do
-    unlogger.py d >> results.csv
-done
+   for d in initialTests/*
+   do
+       ./unlogger.py d >> results.csv
+   done
 
-unlogger.py randomTests/ >> results.csv
+   ./unlogger.py randomTests/ >> results.csv
+else
+    echo "delete or cache the results file so it doesn't get clobbered"
+fi
