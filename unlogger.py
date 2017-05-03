dc168b9b91a9154243bbc4873bac438c5af98a9a
Ian Voysey <iev@cs.cmu.edu>
2017-05-03 14:06:36 -0400

    scraps

--- unlogger.py
+++ unlogger.py
@@ -3,6 +3,7 @@ import json
 import glob
 import sys
 import os
+import csv
 
 # take directory of interest on the command line as the first argument.
 target_dir = sys.argv[1]
@@ -16,9 +17,15 @@ for j_path in glob.glob('%s/*.json' % target_dir):
 
     ## split out the hash
     parts = basename.split('_')
+
+    ## parts[2] ends in a '.json' because python basename isn't posix
+    ## standard, so this grabs just the hash that they use to name each
+    ## test.
     test_name = (parts[2].split('.'))[0]
 
     print test_name
 
-    with open(j_path) as f:
-        data = json.load(f)
+    with open(j_path) as test_json:
+        data = json.load(test_json)
+
+        print (str(data))
