c45d24d4cb92d90dea61651487ab9d3ff26fe06b
Ian Voysey <iev@cs.cmu.edu>
2017-05-08 17:54:48 -0400

    matching run script for the easier to use header file

--- run.sh
+++ run.sh
@@ -2,14 +2,16 @@
 
 if ! [ -e "results.csv" ]
 then
-   cat header.csv >> results.csv
+    # turn header.csv into a csv instead of newline delimited, chopping off
+    # the trailing comma from tr
+   cat header.csv | tr '\n' ',' | sed 's/,$/\n/' >> results.csv
 
    for d in initialTests/*
    do
        ./unlogger.py $d >> results.csv
    done
 
-   ./unlogger.py randomTests/ >> results.csv
+#   ./unlogger.py randomTests/ >> results.csv
 else
     echo "delete or cache the results file so it doesn't get clobbered"
 fi
