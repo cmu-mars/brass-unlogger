f8e30afa03d337f69e672510ce6d9e6e07cc3769
Ian Voysey <iev@cs.cmu.edu>
2017-05-25 14:08:05 -0400

    towards #3

--- make_maps.py
+++ make_maps.py
@@ -4,6 +4,8 @@ import sys
 import os
 import csv
 import svgwrite
+import progressbar
+from PIL import Image, ImageDraw
 
 ## read the results file into a list of dictionaries keyed on the header
 ## names.
@@ -15,20 +17,39 @@ try:
 
 except IOError:
     print "could not open results.csv"
+    sys.exit(1)
 
+try:
+    os.mkdir('images')
+except OSError:
+    print "images directory already exists"
+    sys.exit(1)
 
 ## start: green x
 ## target: green o
 ## final: yellow x
 ## obstacle: red box
 
-    # start x
-    # start y
-    # target x
-    # target y
-    # final x
-    # final y
-    # obstacle x
-    # obstacle y
+bar = progressbar.ProgressBar()
 
-for line in results:
+for line in bar(results):
+    mapbase = Image.open('Wean-entire-floor4.png')
+    draw = ImageDraw.Draw(mapbase)
+
+    draw.line((0, 0) + mapbase.size, fill=128)
+    draw.line((0, mapbase.size[1], mapbase.size[0], 0), fill=128)
+    del draw
+
+    mapbase.save('images/%s.png' % os.path.basename(line['json path']).split('.')[0], "PNG")
+
+    # line['start x']
+    # line['start y']
+
+    # line['target x']
+    # line['target y']
+
+    # line['final x']
+    # line['final y']
+
+    # line['obstacle x']
+    # line['obstacle y']
